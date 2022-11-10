b234cdcfc809a8310c09b3994272905debf866f6
sbj <sbahnj@gmail.com>
2022-11-09 16:23:54 -0800

    tuiter

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import cors from 'cors'
+
 import HelloController
     from "./controllers/hello-controller.js"
 
@@ -8,6 +10,8 @@ import UserController
 
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json());
 
